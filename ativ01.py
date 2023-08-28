6923fdac8bfd6b8c916b848df7dc0492d705db13
brunosuassuna <brunosuassuna.dev@gmail.com>
2023-08-28 17:04:11 -0300

    Enviando primeira atividade

---
+++ ativ01.py
@@ -0,0 +1,8 @@
+nume = int(input("Digite um número"))
+sucessor = 1 + nume
+antecessor = nume - 1 
+print(f"O antecessor do número {nume} é {antecessor} e o sucessor {sucessor}")
+
+print(type(nume))
+print(type(antecessor))
+print(type(sucessor))
